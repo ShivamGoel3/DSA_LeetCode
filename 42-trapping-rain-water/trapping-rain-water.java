824603ae669b60ecf83033cc7bf7b3f4907b3ad0
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-05 02:12:36 +0530

    Time: 1 ms (62.65%) | Memory: 46.9 MB (8.65%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,29 +1,19 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-        int l = 0;
-        for (int i = 0; i < height.length; i++) {
-            l = Math.max(l, height[i]);
-            prefix[i] = l;
-            // System.out.print(l + " ");
-            //l = Math.max(l, height[i]);
-        }
-        // System.out.println(" ");
-        int r = 0;
-        for (int i = height.length - 1; i >= 0; i--) {
-            r = Math.max(r, height[i]);
-            suffix[i] = r;
-            // System.out.print(r + " ");
-
-        }
-        int ans = 0;
-        for (int i = 0; i < height.length; i++) {
-            // System.out.println(prefix[i] + " " + suffix[i]);
-            // if (height[i] == suffix[i] || height[i] == prefix[i])
-            //     continue;
-            ans += Math.min(suffix[i], prefix[i]) - height[i];
+        int i = 0, j = n - 1, ans = 0;
+        int maxl = 0, maxr = 0;
+        while (i < j) {
+            maxl = Math.max(maxl, height[i]);
+            maxr = Math.max(maxr, height[j]);
+            ans += Math.min(maxl, maxr);
+            if (height[i] < height[j]) {
+                ans -= height[i];
+                i++;
+            } else {
+                ans -= height[j];
+                j--;
+            }
         }
         return ans;
     }
