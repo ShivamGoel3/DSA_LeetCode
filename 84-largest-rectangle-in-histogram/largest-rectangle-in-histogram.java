b0d5a24041d456d46ea83f11f38a1f5ed94249ac
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-05 12:34:59 +0530

    Time: 68 ms (63.84%) | Memory: 57.9 MB (28.11%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -2,26 +2,18 @@ class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
         Stack<Integer> s = new Stack<>();
-        int[] nextsmaller = new int[n];
-        int[] prevsmaller = new int[n];
-        for (int i = n - 1; i >= 0; i--) {
-            while (s.size() > 0 && heights[s.peek()] >= heights[i])
-                s.pop();
-            nextsmaller[i] = s.size() == 0 ? n - 1 : s.peek() - 1;
-            s.push(i);
-        }
-        s.clear();
-        for (int i = 0; i < n; i++) {
-            while (s.size() > 0 && heights[s.peek()] >= heights[i])
-                s.pop();
-            prevsmaller[i] = s.size() == 0 ? 0 : s.peek() + 1;
-            s.push(i);
-        }
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            // System.out.println(nextsmaller[i] +" "+prevsmaller[i]);
-            int count = nextsmaller[i] - prevsmaller[i] + 1;
-            ans = Math.max(ans, count * heights[i]);
+        for (int i = 0; i <= n; i++) {
+            while (s.size() > 0 && (i == n || heights[s.peek()] >= heights[i])) {
+                int eleIndex = s.peek();
+                s.pop();
+                int prevsmaller = s.size() == 0 ? 0 : s.peek() + 1;
+                int nextsmaller = i - 1;
+                // System.out.println(nextsmaller[i] +" "+prevsmaller[i]);
+                int count = nextsmaller - prevsmaller + 1;
+                ans = Math.max(ans, count * heights[eleIndex]);
+            }
+            s.push(i);
         }
         return ans;
     }
