505c7d9b7812f3300fda7a12431c8028cc3ea17b
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-08 23:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (7.40%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,25 +1,30 @@
 class Solution {
+    int call(int i, int j, int[][] obstacleGrid, int[][] dp) {
+        if (i < 0 || j < 0)
+            return -1;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (obstacleGrid[i][j] == 1)
+            return dp[i][j] = 0;
+        if (i == 0 && j == 0)
+            return dp[i][j] = 1;
+        int a = call(i - 1, j, obstacleGrid, dp);
+        int b = call(i, j - 1, obstacleGrid, dp);
+        int sum = 0;
+        if (a != -1)
+            sum += a;
+        if (b != -1)
+            sum += b;
+        return dp[i][j] = sum;
+    }
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n = obstacleGrid[0].length, m = obstacleGrid.length;
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else if (i == 0 && dp[i][j - 1] != 0)
-                    dp[i][j] = 1;
-                else if (j == 0 && dp[i - 1][j] != 0)
-                    dp[i][j] = 1;
-                else if (i == 0 || j == 0)
-                    dp[i][j] = 0;
-                else
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
+            for (int j = 0; j < n; j++)
+                dp[i][j] = -1;
         }
-        return dp[m - 1][n - 1];
+        return call(m - 1, n - 1, obstacleGrid, dp);
     }
 }
