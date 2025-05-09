e3dcc6b283863821c071bc2b7cd963fad5ce0431
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-09 21:02:09 +0530

    Time: 4 ms (1.91%) | Memory: 45.4 MB (9.57%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.java
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.java
@@ -4,17 +4,28 @@ class Solution {
         List<String> ans = new ArrayList<>();
         int n = words.length;
         for (int i = 0; i < n; i++) {
-            List<String> a = new ArrayList<>();
+            int index = 0;
+            int size = 0;
             for (int j = 0; j < i; j++) {
                 if (groups[i] != groups[j]) {
-                    if (a.size() < dp.get(j).size())
-                        a = new ArrayList<>(dp.get(j));
+                    if (size < dp.get(j).size()) {
+                        size = dp.get(j).size();
+                        index = j;
+                    }
                 }
             }
-            a.add(words[i]);
-            dp.add(a);
-            if (a.size() > ans.size())
-                ans = a;
+            // a.add(words[i]);
+            if (size != 0) {
+                List<String> a = new ArrayList<>(dp.get(index));
+                a.add(words[i]);
+                dp.add(a);
+            } else {
+                List<String> a = new ArrayList<>();
+                a.add(words[i]);
+                dp.add(a);
+            }
+            if (dp.get(i).size() > ans.size())
+                ans = dp.get(i);
         }
         return ans;
     }
