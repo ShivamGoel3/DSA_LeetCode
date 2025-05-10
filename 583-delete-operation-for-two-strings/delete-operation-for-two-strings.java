4644bb0b193d2ed1f02331e6f1a1f7c2a4684403
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-10 23:06:09 +0530

    Time: 7 ms (72.45%) | Memory: 44.5 MB (92.72%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,41 +1,26 @@
 class Solution {
-    int call(String word1, String word2, int i, int j, int[][] dp) {
-        if (i <= 0)
-            return j;
-        if (j <= 0)
-            return i;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-            return dp[i][j] = call(word1, word2, i - 1, j - 1, dp);
-        } else {
-            int a = call(word1, word2, i, j - 1, dp);
-            int b = call(word1, word2, i - 1, j, dp);
-            return dp[i][j] = 1 + Math.min(a, b);
-        }
-    }
 
     public int minDistance(String word1, String word2) {
         int n = word1.length(), m = word2.length();
         int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0) {
-                    dp[i][j] = j;
-                    continue;
-                } else if (j == 0) {
-                    dp[i][j] = i;
-                    continue;
-                }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // if (i == 0) {
+                //     dp[i][j] = j;
+                //     continue;
+                // } else if (j == 0) {
+                //     dp[i][j] = i;
+                //     continue;
+                // }
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
                     int a = dp[i - 1][j];
                     int b = dp[i][j - 1];
-                    dp[i][j] = 1 + Math.min(a, b);
+                    dp[i][j] = Math.max(a, b);
                 }
             }
         }
-        return dp[n][m];
+        return n + m - 2 * dp[n][m];
     }
 }
