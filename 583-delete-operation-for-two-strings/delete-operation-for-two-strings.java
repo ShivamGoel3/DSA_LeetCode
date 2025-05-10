32af5573d555f370cb6e52ad48b07196bc76b3fe
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-10 23:02:28 +0530

    Time: 7 ms (72.45%) | Memory: 44.6 MB (92.72%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -20,9 +20,22 @@ class Solution {
         int[][] dp = new int[n + 1][m + 1];
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= m; j++) {
-                dp[i][j] = -1;
+                if (i == 0) {
+                    dp[i][j] = j;
+                    continue;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                    continue;
+                }
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int a = dp[i - 1][j];
+                    int b = dp[i][j - 1];
+                    dp[i][j] = 1 + Math.min(a, b);
+                }
             }
         }
-        return call(word1, word2, n, m, dp);
+        return dp[n][m];
     }
 }
