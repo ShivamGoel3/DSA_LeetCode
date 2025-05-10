cb8f159eb0a82e7350022731297f50d03b1f0e4e
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-10 20:49:00 +0530

    Time: 10 ms (50.80%) | Memory: 49.3 MB (39.56%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,28 +1,20 @@
 class Solution {
-    int call(int[] coins, int amount, int index, int[][] dp) {
-        if (amount < 0)
-            return 0;
-        if (index < 0) {
-            if (amount == 0)
-                return 1;
-            else
-                return 0;
-        }
-        if (dp[index][amount] != -1)
-            return dp[index][amount];
-        int a = call(coins, amount, index - 1, dp);
-        int b = call(coins, amount - coins[index], index, dp);
-        return dp[index][amount] = a + b;
-    }
 
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                dp[i][j] = -1;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= coins[i - 1]) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                }
+                // System.out.print(j + " " + dp[i][j] + " ");
             }
+            // System.out.println("");
         }
-        return call(coins, amount, n - 1, dp);
+        return dp[n][amount];
     }
 }
