c652bba545eee70d8ce5fd6655769afffd7a3d6d
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-22 19:42:26 +0530

    Time: 31 ms (17.92%) | Memory: 42.6 MB (59.33%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,38 +1,37 @@
 class Solution {
 
-    int call(int i, int j, int[] cuts, HashMap<String, Integer> dp) {
-        // if (index == cuts.length)
-        //     return 0;
-        // if (cuts[index] < i || cuts[index] > j)
-        //     return 0;
-        if (dp.containsKey("" + i + " " + j))
-            return dp.get("" + i + " " + j);
+    int call(int i, int j, ArrayList<Integer> cut, int[][] dp) {
+        if (j - i == 1)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
         int ans = Integer.MAX_VALUE;
-        boolean check = false;
-        for (int k = 0; k < cuts.length; k++) {
-            // swap(k, index, cuts);
-            if (cuts[k] <= i || cuts[k] >= j)
-                continue;
-            check = true;
-            int b = call(i, cuts[k], cuts, dp);
-            int c = call(cuts[k], j, cuts, dp);
-            ans = Math.min(ans, b + c + j - i);
+        for (int k = i + 1; k < j; k++) {
+            // System.out.println(i + " " + k + " " + j);
+            int b = call(i, k, cut, dp);
+            int c = call(k, j, cut, dp);
+            ans = Math.min(ans, b + c + cut.get(j) - cut.get(i));
             // System.out.println(i + " " + j + " " + k + " " + b + " " + c);
-            // swap(k, index, cuts);
         }
-        int a = check ? ans : 0;
-        dp.put("" + i + " " + j, a);
+        int a = ans != Integer.MAX_VALUE ? ans : 0;
+        dp[i][j] = a;
         return a;
     }
 
     public int minCost(int n, int[] cuts) {
-        // int[][] dp = new int[n + 1][n + 1];
-        // for (int i = 0; i <= n; i++) {
-        //     for (int j = 0; j <= n; j++) {
-        //         dp[i][j] = -1;
-        //     }
-        // }
-        HashMap<String, Integer> dp = new HashMap<>();
-        return call(0, n, cuts, dp);
+        ArrayList<Integer> cut = new ArrayList<>();
+        for (int i = 0; i < cuts.length; i++) {
+            cut.add(cuts[i]);
+        }
+        cut.add(0);
+        cut.add(n);
+        Collections.sort(cut);
+        int[][] dp = new int[cut.size()][cut.size()];
+        for (int i = 0; i < cut.size(); i++) {
+            for (int j = 0; j < cut.size(); j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return call(0, cut.size() - 1, cut, dp);
     }
 }
