393f32f5d05b94463071ae5b057278914454b99d
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-22 19:46:38 +0530

    Time: 19 ms (72.59%) | Memory: 42.5 MB (71.90%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,23 +1,5 @@
 class Solution {
 
-    int call(int i, int j, ArrayList<Integer> cut, int[][] dp) {
-        if (j - i == 1)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int ans = Integer.MAX_VALUE;
-        for (int k = i + 1; k < j; k++) {
-            // System.out.println(i + " " + k + " " + j);
-            int b = call(i, k, cut, dp);
-            int c = call(k, j, cut, dp);
-            ans = Math.min(ans, b + c + cut.get(j) - cut.get(i));
-            // System.out.println(i + " " + j + " " + k + " " + b + " " + c);
-        }
-        int a = ans != Integer.MAX_VALUE ? ans : 0;
-        dp[i][j] = a;
-        return a;
-    }
-
     public int minCost(int n, int[] cuts) {
         ArrayList<Integer> cut = new ArrayList<>();
         for (int i = 0; i < cuts.length; i++) {
@@ -27,11 +9,18 @@ class Solution {
         cut.add(n);
         Collections.sort(cut);
         int[][] dp = new int[cut.size()][cut.size()];
-        for (int i = 0; i < cut.size(); i++) {
-            for (int j = 0; j < cut.size(); j++) {
-                dp[i][j] = -1;
+
+        for (int len = 2; len < cut.size(); len++) {
+            int j = len;
+            for (int i = 0; i < cut.size() && j < cut.size(); i++, j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j],
+                            dp[i][k] + dp[k][j] + cut.get(j) - cut.get(i));
+                }
             }
         }
-        return call(0, cut.size() - 1, cut, dp);
+
+        return dp[0][cut.size() - 1];
     }
 }
