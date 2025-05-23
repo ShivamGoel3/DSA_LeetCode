df56312f24e985e521f82a85d77cb777a83b5c1c
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-05-23 19:06:29 +0530

    Time: 373 ms (5.01%) | Memory: 45.7 MB (15.13%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,40 @@
+class Solution {
+    boolean check(HashMap<Character, Integer> m1, HashMap<Character, Integer> m2) {
+        for (Character i : m1.keySet()) {
+            int freq = m1.get(i);
+            if (m2.getOrDefault(i, 0) < freq)
+                return false;
+        }
+        return true;
+    }
+
+    public String minWindow(String s, String t) {
+        int start = 0;
+        int ans = Integer.MAX_VALUE;
+        HashMap<Character, Integer> m1 = new HashMap<>();
+        HashMap<Character, Integer> m2 = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            m1.put(ch, m1.getOrDefault(ch, 0) + 1);
+        }
+        // System.out.println(m1);
+        String a = "";
+        for (int end = 0; end < s.length(); end++) {
+            char ch = s.charAt(end);
+            m2.put(ch, m2.getOrDefault(ch, 0) + 1);
+            while (check(m1, m2)) {
+                if (ans > (end - start + 1)) {
+                    ans = end - start + 1;
+                    a = s.substring(start, end + 1);
+                }
+                char w = s.charAt(start);
+                m2.put(w, m2.get(w) - 1);
+                if (m2.get(w) == 0)
+                    m2.remove(w);
+                start++;
+            }
+        }
+        return a;
+
+    }
+}
