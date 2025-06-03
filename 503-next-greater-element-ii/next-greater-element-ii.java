0537d784c744f06a7857c01adca830467fed44f8
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-06-03 11:56:49 +0530

    Time: 18 ms (28.97%) | Memory: 46.5 MB (28.09%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -19,20 +19,11 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         Stack<Integer> s = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (s.size() > 0 && s.peek() <= nums[i])
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (s.size() > 0 && nums[s.peek()] <= nums[i % n])
                 s.pop();
-            ans[i] = s.size() == 0 ? Integer.MIN_VALUE : s.peek();
-            s.push(nums[i]);
-        }
-        int max = 0;
-        ArrayList<Integer> a = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (ans[i] == Integer.MIN_VALUE) {
-                ans[i] = find(a, nums[i]);
-            }
-            if (a.size() == 0 || a.get(a.size() - 1) <= nums[i])
-                a.add(nums[i]);
+            ans[i % n] = s.size() == 0 ? -1 : nums[s.peek()];
+            s.push(i % n);
         }
         return ans;
     }
