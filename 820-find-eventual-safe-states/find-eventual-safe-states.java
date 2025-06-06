506902065e4e560a46c9eb4acf4adb3cdc4c95b9
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-06-06 13:33:36 +0530

    Time: 4 ms (98.72%) | Memory: 55.6 MB (56.28%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,16 +1,14 @@
 class Solution {
-    boolean call(int[][] graph, int node, int[] visited, int[] res, int[] rank) {
+    boolean call(int[][] graph, int node, int[] visited, int[] res) {
         visited[node] = 1;
         res[node] = 1;
         for (int i = 0; i < graph[node].length; i++) {
             if (visited[graph[node][i]] == 0) {
-                boolean check = call(graph, graph[node][i], visited, res, rank);
+                boolean check = call(graph, graph[node][i], visited, res);
                 if (check) {
-                    rank[graph[node][i]] = 1;
                     return true;
                 }
-            } else if (res[graph[node][i]] == 1 || rank[graph[node][i]] == 1) {
-                rank[graph[node][i]] = 1;
+            } else if (res[graph[node][i]] == 1) {
                 return true;
             }
         }
@@ -21,18 +19,15 @@ class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> ans = new ArrayList<>();
         int n = graph.length;
-        int[] rank = new int[n];
+        int[] res = new int[n];
         int[] visited = new int[n];
         for (int i = 0; i < n; i++) {
             if (visited[i] == 1)
                 continue;
-            int[] res = new int[n];
-            boolean c = call(graph, i, visited, res, rank);
-            if (c)
-                rank[i] = 1;
+            call(graph, i, visited, res);
         }
         for (int i = 0; i < n; i++) {
-            if (rank[i] == 0)
+            if (res[i] == 0)
                 ans.add(i);
         }
         return ans;
