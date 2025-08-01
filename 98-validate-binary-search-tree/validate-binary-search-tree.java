52a10177045136b87a9a47417eb172441727bcd5
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-08-01 19:16:19 +0530

    Time: 1 ms (18.63%) | Memory: 43.2 MB (88.58%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,24 +14,31 @@
  * }
  */
 class Solution {
-    TreeNode prev = null;
-
-    boolean call(TreeNode root) {
-        if (root == null)
-            return true;
-        boolean check = call(root.left);
-        // System.out.println(root + " " + prev + " " + root.val);
-        if (check == false)
-            return false;
-        if (prev != null) {
-            if (prev.val >= root.val)
-                return false;
-        }
-        prev = root;
-        return call(root.right);
-    }
-
     public boolean isValidBST(TreeNode root) {
-        return call(root);
+        TreeNode prev = null;
+        while (root != null) {
+            if (root.left != null) {
+                TreeNode temp = root.left;
+                while (temp.right != null && temp.right != root) {
+                    temp = temp.right;
+                }
+                if (temp.right == null) {
+                    temp.right = root;
+                    root = root.left;
+                } else {
+                    if (prev != null && prev.val >= root.val)
+                        return false;
+                    prev = root;
+                    root = temp.right.right;
+                    temp.right = null;
+                }
+            } else {
+                if (prev != null && prev.val >= root.val)
+                    return false;
+                prev = root;
+                root = root.right;
+            }
+        }
+        return true;
     }
 }
