e9b8b761c6a63f84f6114cd0c0fbd15c8cc77e7d
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-08-09 17:42:43 +0530

    Time: 2 ms (47.92%) | Memory: 43.6 MB (26.05%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,20 +1,19 @@
 class Solution {
-    void call(int n, String s , List<String>ans,int open, int close){
-        if(close>open)
-        return;
-        if(open>n || close>n)
-        return;
-        if(open ==n && close==n){
+    void call(List<String> ans, int n, int open, int close, String s) {
+        if (close > open || open > n)
+            return;
+        if (open == close && open + close == 2 * n) {
             ans.add(s);
             return;
         }
-        call(n,s+"(",ans,open+1,close);
-        call(n,s+")",ans,open,close+1);
+        call(ans, n, open + 1, close, s + "(");
+        call(ans, n, open, close + 1, s + ")");
     }
+
     public List<String> generateParenthesis(int n) {
-        List<String>ans = new ArrayList<>();
-        int open = 0;int close = 0;
-        call(n,"", ans,open,close);
+        List<String> ans = new ArrayList<>();
+        int opening = 0, closing = 0;
+        call(ans, n, opening, closing, "");
         return ans;
     }
 }
