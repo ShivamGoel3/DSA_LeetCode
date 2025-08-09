dce33d7fd1a733f5476ef4786cc89163a5d549c0
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2025-08-09 18:12:51 +0530

    Time: 25 ms (86.11%) | Memory: 45.6 MB (55.32%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0)
+            return false;
         HashMap<Integer, Integer> m = new HashMap<>();
         for (int i = 0; i < hand.length; i++) {
             m.put(hand[i], m.getOrDefault(hand[i], 0) + 1);
         }
-
+        Arrays.sort(hand);
         for (int i = 0; i < hand.length; i++) {
-            int startcard = hand[i];
-            while (m.getOrDefault(startcard, 0) > 0)
-                startcard--;
-            while (startcard <= hand[i]) {
-                while (m.getOrDefault(startcard, 0) > 0) {
-                    for (int j = startcard; j < startcard + groupSize; j++) {
-                        if (m.getOrDefault(j, 0) == 0)
-                            return false;
-                        m.put(j, m.get(j) - 1);
-                    }
-                }
-                startcard++;
+            int a = hand[i];
+            int count = 0;
+            while (m.getOrDefault(a, 0) > 0 && count < groupSize) {
+                m.put(a, m.get(a) - 1);
+                a++;
+                count++;
             }
+            // System.out.println(hand[i] + " " + a + " " + count);
+            if (count != 0 && count < groupSize)
+                return false;
         }
         return true;
     }
