07f54d0aad02320a8f3eb820c519209c3f5ca975
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2026-01-06 15:49:49 +0530

    Time: 2 ms (98.83%) | Memory: 45.2 MB (42.52%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder str = new StringBuilder();
-        for (int k = 0; k < s.length(); k++) {
-            if (s.charAt(k) >= 97 && s.charAt(k) <= 122) {
-                str.append(s.charAt(k));
-            } else if (s.charAt(k) >= 65 && s.charAt(k) <= 90) {
-                str.append((char) (s.charAt(k) + 32));
-            } else if (s.charAt(k) >= 48 && s.charAt(k) <= 57) {
-                str.append(s.charAt(k));
-            }
-        }
-        int i = 0, j = str.length() - 1;
+        int i = 0, j = s.length() - 1;
         while (i < j) {
-            if (str.charAt(i) != str.charAt(j))
-                return false;
-            i++;
-            j--;
+            if (!((s.charAt(i) >= 97 && s.charAt(i) <= 122) ||
+                    (s.charAt(i) >= 65 && s.charAt(i) <= 90) ||
+                    (s.charAt(i) >= 48 && s.charAt(i) <= 57))) {
+                i++;
+            } else if (!((s.charAt(j) >= 97 && s.charAt(j) <= 122) ||
+                    (s.charAt(j) >= 65 && s.charAt(j) <= 90) ||
+                    (s.charAt(j) >= 48 && s.charAt(j) <= 57))) {
+                j--;
+            } else {
+                char a = s.charAt(i);
+                char b = s.charAt(j);
+                if (a >= 65 && a <= 90) {
+                    a = (char) (a + 32);
+                }
+                if (b >= 65 && b <= 90) {
+                    b = (char) (b + 32);
+                }
+                if (a != b)
+                    return false;
+                i++;
+                j--;
+            }
         }
         return true;
     }
